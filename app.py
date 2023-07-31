806763a93fc5adf3eb9a21b0b220e5bbb89fee3a
alexis perez <alexisperez99@hotmail.fr>
2023-07-31 12:34:16 +0200

    testing connection main

--- app.py
+++ app.py
@@ -40,3 +40,4 @@ def predict_species(sepal_length: Optional[float] = None, sepal_width: Optional[
 # #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000, reload = True)
+    # test
