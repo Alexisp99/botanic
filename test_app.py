d04b03a4024845362e086629e2b2f9ca686f656d
alexis perez <alexisperez99@hotmail.fr>
2023-07-31 14:45:16 +0200

    solve test unit

--- test_app.py
+++ test_app.py
@@ -7,4 +7,4 @@ from model import IrisModel, IrisSpecies
 class TestIrisModel(unittest.TestCase):
    def test_model_initialization(self):
        new_model = IrisModel()
-       self.assertIn('iris_model.joblib',new_model.model_fname_)
+       self.assertIn('iris_model.pkl',new_model.model_fname_)
